b7479754d4139a0f3d22e565f1f80ac4ccca3ee4
Alexandru Mosoi <brtzsnr@gmail.com>
2008-09-06 12:00:53 +0000

    blah
    git-svn-id: svn://rosedu.org/vmchecker/trunk@81 6be24367-e808-40b8-950b-c7034a306bf9

--- bin/misc.py
+++ bin/misc.py
@@ -19,6 +19,21 @@ def find_config_file(file_name='vmchecker.ini'):
 	while cwd != '/':
 		# XXX assuming script runs on *nix, stops at root
 		path = os.path.join(cwd, file_name)
-		if os.isfile(path):
-			return os.path.abspath(path):
+		if os.path.isfile(path):
+			return os.path.abspath(path)
+		cwd = os.path.dirname(cwd)
 
+
+def get_option(config, homework, option):
+	"""Given homework name, returns ip of remote testing machine.
+	If there is no such option, returns None."""
+
+	assert config.has_section(homework), 'No such homework %s' % homework
+
+	# first tries to get option from `homework` section
+	if config.has_option(homework, option):
+		return config.get(homework, option)
+
+	# falls back to default remote ip
+	if config.has_option('DEFAULT', option):
+		return config.get('DEFAULT', default)
--- bin/remote_check.py
+++ bin/remote_check.py
@@ -5,21 +5,58 @@
 
 __author__ = 'Alexandru Mosoi, brtzsnr@gmail.com'
 
-import optparse
+import sys
+import os
+import subprocess
+import ConfigParser
 
+import misc
 
-
-cmdline = optparse.OptionParser(version='%prog 0.1')
-cmdline.add_option('--homework', type='string', dest='homework',
-                   help='fișierul de configurare a temei')
-cmdline.add_option('--ip', type='string', dest='ip',
-                   help='adresa IP a sistemului de testare')
-cmdline.add_option('--path', type='string', dest='path',
-                   help='calea de stocare la distanta a fisierelor')
 
 def main():
-    options = cmdline.parse_args()[0]
+    if len(sys.argv) == 1:
+        print >> sys.stderr, 'Usage: %s homework_config_file' % sys.argv[0]
+        sys.exit(1)
 
+    config_file = misc.find_config_file('vmchecker.ini')
+
+    homework = ConfigParser.RawConfigParser()
+    homework.read(sys.argv[1])
+
+    config = ConfigParser.RawConfigParser()
+    config.read(config_file)
+
+    job = homework.get('DEFAULT', 'Job')
+
+    remote_ip = misc.get_option(config, job, 'RemoteIP')
+    assert remote_ip, 'No ip for remote machine'
+
+    remote_queue = misc.get_option(config, job, 'RemoteQueue')
+    assert remote_queue, 'No queue on remote machine'
+
+    remote_user = misc.get_option(config, job, 'RemoteUser')
+    assert remote_user, 'No remote user supplied'
+
+    remote_notifier = misc.get_option(config, job, 'RemoteNotifier')
+    assert remote_notifier, 'No notifier supplied'
+
+    return_code = subprocess.call([
+        'scp',           # program to invoke
+        sys.argv[1],     # config file to copy 
+        '%s@%s:%s' % (remote_user, remote_ip, remote_queue)])
+    if return_code != 0:
+        print >> sys.stderr, 'Eroare la copierea temei pe masina de testare'
+        sys.exit(1)
+
+    return_code = subprocess.call([
+        'ssh',
+        '%s@%s' % (remote_user, remote_ip),
+        os.path.join(remote_queue, remote_notifier)])
+    if return_code != 0:
+        print >> sys.stderr, 'Nu am putut invoca programul de notificare'
+        sys.exit(1)
+
+    print >> sys.stderr, 'Tema a fost copiata cu succes'
 
 
 if __name__ == '__main__':
