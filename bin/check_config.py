eee2f3b73f11c9ba6f7ac7684ec2873dbb6d09b3
Ana Savu <ana.savu86@gmail.com>
2008-09-06 17:54:08 +0000

    git-svn-id: svn://rosedu.org/vmchecker/trunk@112 6be24367-e808-40b8-950b-c7034a306bf9

--- bin/check_config.py
+++ bin/check_config.py
@@ -12,24 +12,26 @@ import misc
 import ConfigParser
 import subprocess
 import os
+import shutil
 
 
 def main():
-    if len(sys.argv) != 3:
-        print >> sys.stderr, 'Usage: %s user_id assignment_id archive_path results_path' % sys.argv[0]
+    if len(sys.argv) != 4:
+        print >> sys.stderr, 'Usage: %s user_id assignment_id archive_path' % sys.argv[0]
         sys.exit(1)
 
     course_config_file = misc.config_file()
 
     user_id = sys.argv[1]             # student name
     job = sys.argv[2]                 # assignment name
+    archive_path = sys.argv[3]        # archive path
 
     global_config_file = ConfigParser.RawConfigParser()
     global_config_file.readfp(open(course_config_file))
 
     # parses global course configuration file
     course_name = global_config_file.get('DEFAULT', 'CourseName')
-    base_path = global_config_file.get('DEFAULT', 'BasePath')
+    # XXX base_path = global_config_file.get('DEFAULT', 'BasePath')
     penalty = global_config_file.get('DEFAULT', 'Penalty')
 
     tester = global_config_file.get(job, 'Tester')
@@ -38,13 +40,19 @@ def main():
     kernel_msg = misc.get_option(global_config_file, job, 'KernelMsg')
 
     # the upload time is the system's current time
-    upload_time = time.strftime("%m-%d-%y %H:%M")
+    upload_time = time.strftime("%d-%m-%y %H:%M:%S")
+
+    hw_path = os.path.join(misc.vmchecker_root(), 'back', job, user_id, upload_time)
+    os.makedirs(hw_path)
+    
+    hw_path = os.path.join(hw_path, 'file.zip')
+    shutil.copy(archive_path, hw_path)
 
     # assignment configuration file
     file = '[DEFAULT]\n'
     file += 'Job=%s\n' % job
     file += 'UserId=%s\n' % user_id
-    file += 'BasePath=%s\n' % base_path
+    file += 'VMCheckerRoot=%s\n' % misc.vmchecker_root()
     file += 'Penalty=%s\n' % penalty
     file += 'Tester=%s\n' % tester
     file += 'VMName=%s\n' % vmname
@@ -63,13 +71,14 @@ def main():
     with open(job_config_file, 'w') as handle:
         handle.write(file);
 
+    # call remote_check script
     return_code = subprocess.call([
         './remote_check.py',
         '%s' % job_config_file])
+
     if return_code != 0:
         print >> sys.stderr, 'Nu am putut invoca programul remote_check'
-        sys.exit(1)
-
+        sys.exit(1)    
 
 if __name__ == '__main__':
     main()
