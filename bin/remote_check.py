25ae0a4f58dabe90404c2894e9ae547f7b0c01d8
Alexandru Mosoi <brtzsnr@gmail.com>
2008-09-06 10:25:01 +0000

    git-svn-id: svn://rosedu.org/vmchecker/trunk@73 6be24367-e808-40b8-950b-c7034a306bf9

---
+++ bin/remote_check.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+# -*- coding: UTF-8 -*-
+# vim: set expandtab :
+
+import optparse
+
+
+__author__ = 'Alexandru Mosoi, brtzsnr@gmail.com'
+
+
+cmdline = optparse.OptionParser(version='%prog 0.1')
+cmdline.add_option('--homework', type='string', dest='homework',
+                   help='fișierul de configurare a temei')
+cmdline.add_option('--ip', type='string', dest='ip',
+                   help='adresa IP a sistemului de testare')
+cmdline.add_option('--path', type='string', dest='path',
+                   help='calea de stocare la distanta a fisierelor')
+
+def main():
+    options = cmdline.parse_args()[0]
+
+
+
+if __name__ == '__main__':
+    main()
