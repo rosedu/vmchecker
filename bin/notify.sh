9fb446fbd45520e0b9f226d3fcbc3919baf2aec6
Alexandru Mosoi <brtzsnr@gmail.com>
2008-09-07 07:47:37 +0000

    fixed conflict
    git-svn-id: svn://rosedu.org/vmchecker/trunk@123 6be24367-e808-40b8-950b-c7034a306bf9

--- bin/notify.sh
+++ bin/notify.sh
@@ -3,12 +3,9 @@
 # Lucian Adrian Grijincu (lucian.grijincu@gmail.com)
 
 if [ "x$1" = "x" ]; then
-    echo "$0: missing course_id argument"
-    echo "    correct syntax: $0 course_id"
+    echo "Usage: $0 course_id"
     exit 1
 fi
 
-echo "NOTIFY: calling semctl up" $1
+echo "received new homework for" $1
 ./semctl up $1
-
-exit 0
