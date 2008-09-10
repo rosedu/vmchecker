0575fa6db6fb1ac8778dae2f4334d188761d31be
Gheorghe Claudiu-Dan <claudiugh@gmail.com>
2008-09-10 07:24:13 +0000

    create_db() added
    git-svn-id: svn://rosedu.org/vmchecker/trunk@172 6be24367-e808-40b8-950b-c7034a306bf9

--- bin/initialise_course.py
+++ bin/initialise_course.py
@@ -5,8 +5,29 @@ __author__ = 'Ana Savu, ana.savu86@gmail.com'
 
 
 import os
+import sqlite3
 import misc
 
+def create_db(db_path):
+    db_conn = sqlite3.connect(db_path)
+    db_cursor = db_conn.cursor()
+    db_cursor.executescript("""
+	CREATE TABLE studenti 
+		(id INTEGER PRIMARY KEY, 
+		nume TEXT);
+	CREATE TABLE teme 
+		(id INTEGER PRIMARY KEY,
+		nume TEXT,
+		deadline DATE);
+	CREATE TABLE note 
+		(id INTEGER PRIMARY KEY, 
+		id_student INTEGER, 
+		id_tema INTEGER,
+		nota INTEGER,
+		data TIMESTAMP default CURRENT_TIMESTAMP);
+	""")
+    db_cursor.close()
+    db_conn.close()
 
 def main():
     
@@ -32,6 +53,12 @@ def main():
         'Directorul %s exista' % (
             os.path.join(base_path, 'tests')))
     os.mkdir(os.path.join(base_path, 'tests'))
+    
+    # check for DB existance 
+    if None == misc.db_file():
+        create_db(os.path.join(base_path, misc.VMCHECKER_DB))
+    else:
+        print misc.db_file(), " already created"
 
 if __name__ == '__main__':
     main()
