a58d8c2fd44292847e7f929822697fa9aabc06ab
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2008-09-28 17:55:51 +0000

    added more verbose messages
    git-svn-id: svn://rosedu.org/vmchecker/trunk@262 6be24367-e808-40b8-950b-c7034a306bf9

--- bin/notify.sh
+++ bin/notify.sh
@@ -3,9 +3,9 @@
 # Lucian Adrian Grijincu (lucian.grijincu@gmail.com)
 
 if [ -z $1 ]; then
-    echo "Usage: $0 course_id"
+    echo "[NOTIFY.SH] Usage: $0 course_id"
     exit 1
 fi
 
-echo "received new homework for $1"
+echo "[NOTIFY.SH] received new homework for $1"
 `dirname $0`/semctl up $1
