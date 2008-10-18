78e0fc469a3a706a1ab6c54d519724de1558c12a
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2008-10-18 22:39:57 +0000

    git-svn-id: svn://rosedu.org/vmchecker/trunk@290 6be24367-e808-40b8-950b-c7034a306bf9

--- bin/check_latest_versions.py
+++ bin/check_latest_versions.py
@@ -16,12 +16,12 @@ def main():
             dates = os.listdir(namedir)
             dates.sort()
             for date in dates:
-                print ("mata: " + i)
+                print ("mata: " + date)
                 datedir = namedir + "/" + date
                 zipfile = datedir + "/" + "file.zip"
                 print (os.stat(zipfile))
                 if os.stat(zipfile) != None:
-                    print ("\t\t latest date of upload: " + dates[0])
+                    print ("\t\t latest date of upload: " + date)
                     check_config.check_config(name, hw, datedir + "/file.zip")
                     break
 
