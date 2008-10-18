a0ce84b743c205de1e27e4804bda0081942acd65
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2008-10-18 22:10:35 +0000

    a:
    git-svn-id: svn://rosedu.org/vmchecker/trunk@283 6be24367-e808-40b8-950b-c7034a306bf9

--- bin/check_latest_versions.py
+++ bin/check_latest_versions.py
@@ -3,7 +3,11 @@
 import misc
 import os
 
-root = misc.vmchecker_root()
-back = root + "/back"
-for hw in os.listdir(back):
-    print ("hw: " + hw)
+def main():
+    root = misc.vmchecker_root()
+    back = root + "/back"
+    for hw in os.listdir(back):
+        print ("hw: " + hw)
+
+if __name__ == "__main__":
+    main()
