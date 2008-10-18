e6b3054457d8b47e249f4598a8e50f88f5c11617
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2008-10-18 22:14:34 +0000

    git-svn-id: svn://rosedu.org/vmchecker/trunk@284 6be24367-e808-40b8-950b-c7034a306bf9

--- bin/check_latest_versions.py
+++ bin/check_latest_versions.py
@@ -8,6 +8,13 @@ def main():
     back = root + "/back"
     for hw in os.listdir(back):
         print ("hw: " + hw)
+        hwdir = back + hw
+        for name in os.listdir(hwdir):
+            print ("\t name: " + name)
+            namedir = hwdir + name
+            dates = os.listdir(namedir).sort()
+            print ("\t\t latest date of upload: " + dates[0])
+            
 
 if __name__ == "__main__":
     main()
