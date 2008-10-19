12b908bb631acbaa40e03fd4c808b97b21af0b4a
Adriana Szekeres <aaa.szeke@gmail.com>
2008-10-19 17:53:28 +0000

    cleanup in Executor
    git-svn-id: svn://rosedu.org/vmchecker/trunk@310 6be24367-e808-40b8-950b-c7034a306bf9

--- VMExecutor/apelare.sh
+++
@@ -1,3 +0,0 @@
-./vm_executor "lin" "0"  '/home/student/VirtualMachines/Ubuntu/Ubuntu.vmx' '10.38.249.255' 'user' 'user' '/home/user/' '/bin/bash' '/home/user' '/home/student/curs1' 'tema1Lin'
-
-echo $?
