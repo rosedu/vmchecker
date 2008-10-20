3b8def0e42e5d7d6130d2abad6722cd07a939790
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2008-10-20 21:25:36 +0000

    added a new penalty function (used for CPL)
    git-svn-id: svn://rosedu.org/vmchecker/trunk@324 6be24367-e808-40b8-950b-c7034a306bf9

--- bin/penalty.py
+++ bin/penalty.py
@@ -16,7 +16,7 @@
 #   days_late = number of days exceeding the deadline
 
 
-__author__='Ana Savu, ana.savu86@gmail.com'
+__author__='Ana Savu <ana.savu86@gmail.com>, Lucian Adrian Grijincu <lucian.grijincu@gmail.com>'
 
 
 import sys
@@ -25,9 +25,7 @@ import math
 
 
 def parse_time(upload_time_str, deadline_str):
-
     # parse input strngs according to format
-
     upload_time = time.strptime(upload_time_str, "%d-%m-%y %H:%M:%S")
     deadline = time.strptime(deadline_str, "%d-%m-%y %H:%M:%S")
 
@@ -35,12 +33,11 @@ def parse_time(upload_time_str, deadline_str):
 
 
 def compute_penalty(upload_time, deadline, penalty, wheights, limit):
-
-# penalty - for every day after the deadline the product of the penalty and 
-#   the wheight is added to the penalty_points variable
-# wheights - the penalty's wheight per day (the last wheight from the list 
-# is used for subsequent computations)
-# limit - the limit for the penalty value
+    # penalty - for every day after the deadline the product of the penalty and 
+    #   the wheight is added to the penalty_points variable
+    # wheights - the penalty's wheight per day (the last wheight from the list 
+    # is used for subsequent computations)
+    # limit - the limit for the penalty value
 
     # time interval between deadline and upload time (seconds)
     interval = time.mktime(upload_time) - time.mktime(deadline)
@@ -66,44 +63,51 @@ def compute_penalty(upload_time, deadline, penalty, wheights, limit):
     return (min(penalty_points, limit), days_late)
 
 
+
+def compute_penalty_fixed_penalty(upload_time, deadline):
+    # if the number of days pass the deadline exceeds 'x' the homework is
+    # not graded (x = len(wheights) - 1)
+    return compute_penalty(upload_time, deadline, 1, [2, 0, 0, 0, 0, 0, 9], 10)
+
+
 def compute_penalty_linear(upload_time, deadline):
-    
     # for every day pass the deafline the penalty value is added
     # to the penalty_points
     return compute_penalty(upload_time, deadline, 0.25, [1], 3)
 
 
 def compute_penalty_fixed_deadline(upload_time, deadline):
-
     # if the number of days pass the deadline exceeds 'x' the homework is
     # not graded (x = len(wheights) - 1)
     return compute_penalty(upload_time, deadline, 1, [1, 1, 1, 7], 10)
 
 
 def compute_penalty_wheighted(upload_time, deadline):
-
     # the wheight for penalty is diferent depending on the day
     return compute_penalty(upload_time, deadline, 1, [1, 2, 3, 4, 0], 10)
 
-def time_late(upload_time, deadline):
+
+def verbose_time_difference(upload_time, deadline):
     interval = time.mktime(upload_time) - time.mktime(deadline)
     penalty_points = 0
 
-    # only if the number of days late is positive (deadline exceeded)
-    if interval > 0:
-        days_late = int(math.floor(interval / (3600 * 24)))
-        interval -= days_late * (3600 * 24)
-        hour_late = int(math.floor((interval / 3600)))
-        interval -= hour_late * 3600
-        mins_late = int(math.floor((interval / 60)))
-        secs_late = interval - mins_late * 60
-        return " %d zile %d ore %d minute %d secunde" % (days_late, 
-                                                         hour_late,
-                                                         mins_late,
-                                                         secs_late)
+    if interval < 0:
+        str = "inainte de deadline cu "
+        interval = - interval
     else:
-        days_late = 0
-        return " tema trimisa inainte de expirarea deadline-ului"
+        str = "intarziat "
+
+    days_late = int(math.floor(interval / (3600 * 24)))
+    interval -= days_late * (3600 * 24)
+    hour_late = int(math.floor((interval / 3600)))
+    interval -= hour_late * 3600
+    mins_late = int(math.floor((interval / 60)))
+    secs_late = interval - mins_late * 60
+    return str + " %d zile %d ore %d minute %d secunde" % (days_late, 
+                                                           hour_late,
+                                                           mins_late,
+                                                           secs_late)
+
         
     
 def main():
@@ -119,15 +123,16 @@ def main():
 
     # based on the type of penalty computing used call one of 
     # the functions below
-    (penalty_points, days_late) = compute_penalty_linear(upload_time, deadline)
+    # (penalty_points, days_late) = compute_penalty_linear(upload_time, deadline)
     # (penalty_points, days_late) = compute_penalty_fixed_deadline(upload_time, deadline)
+    (penalty_points, days_late) = compute_penalty_fixed_penalty(upload_time, deadline)
     # (penalty_points, days_late) = compute_penalty_wheighted(upload_time, deadline)
     # (penalty_points, days_late) = compute_penalty(upload_time, deadline, my_penalty, 
     #         my_wheights, my_limit)
 
     
     #if penalty_points > 0:
-    print "-%.2f: %s" % (penalty_points, time_late(upload_time, deadline))
+    print "-%.2f: %s" % (penalty_points, verbose_time_difference(upload_time, deadline))
     
 if __name__ == '__main__':
     main()
