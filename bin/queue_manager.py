4fd1ccb1b352f3dadc94321671a60e44ca54cc63
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2009-03-28 23:07:12 +0200

    process stale jobs

--- bin/queue_manager.py
+++ bin/queue_manager.py
@@ -2,7 +2,7 @@
 """Queue manager
 This module depends on pyinotify: http://pyinotify.sourceforge.net/
 It should:
-  * listen for new files on a directory, 
+  * listen for new files on a directory,
   * decompress the archives to a temporary directory,
   * pass path of the directory to commander,
   * waits for the commander to finish.
@@ -16,6 +16,9 @@ import tempfile
 import shutil
 import misc
 import vmcheckerpaths
+import logging
+import os
+import time
 
 from subprocess import check_call
 from os.path import join
@@ -32,26 +35,51 @@ class _QueueManager(ProcessEvent):
 
 
 def _process_job(path, name):
-    location = tempfile.mkdtemp(prefix='vmchecker-', 
+    location = tempfile.mkdtemp(prefix='vmchecker-',
                                 dir=vmcheckerpaths.dir_tester_unzip_tmp())
     archive = join(path, name)
     try:
-        print 'Expanding archive `%s\' at `%s\'.' % (archive, location)
+        logger.info('Expanding archive `%s\' at `%s\'.' % (archive, location))
         check_call(['unzip', '-d', location, archive])
-        
-        print 'Calling commander'
+
+        logger.info('Calling commander for [%s]' % location)
         check_call(['./commander.py', location])
+    except:
+        logger.exception('Caught exception while processing [%s]' % location)
     finally:
-        print 'Cleaning `%s\'' % location
+        logger.info('Cleaning [%s]' % location)
         shutil.rmtree(location)
-    
+
+
+
+def _process_stale_jobs(dir_queue):
+    stale_jobs = os.listdir(dir_queue)
+    if len(stale_jobs) == 0:
+        logger.info('No stale jobs in queue dir [%s]' % dir_queue)
+    for stale_job in stale_jobs:
+        logger.info('Processing stale job [%s] in queue dir %s' % (
+                stale_job, dir_queue))
+        _process_job(dir_queue, stale_job)
 
 def main():
+    dir_queue = vmcheckerpaths.dir_queue()
+    if not os.path.isdir(dir_queue):
+        logger.error('Queue direcotry [%s] missing')
+        exit(1)
+
+    # register for inotify envents before processing stale jobs
     wm = WatchManager()
     notifier = Notifier(wm, _QueueManager())
-    wm.add_watch(vmcheckerpaths.dir_queue(), EventsCodes.ALL_FLAGS['IN_CLOSE_WRITE'])
+    wm.add_watch(dir_queue, EventsCodes.ALL_FLAGS['IN_CLOSE_WRITE'])
+    
+    _process_stale_jobs(dir_queue)
+
+    # set callback to receive notifications
     notifier.loop(callback=lambda self: self.proc_fun())
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger("vmchecker.queue_manager")
+
     main()
