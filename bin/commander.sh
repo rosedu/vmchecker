4e452652978ffc811c8e741c81b1f5da908292cc
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2009-03-29 21:26:44 +0300

    remove bash scripts

--- bin/commander.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "======================================================"
-echo "======================================================"
-
-echo "[COMMANDER.SH] called with " $1
-exec $VMCHECKER_ROOT/Commander/commander "$1"
