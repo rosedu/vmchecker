c914c90d4322bb9a4ff1371691ae20411f755b8c
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2009-03-29 15:54:05 +0300

    add test for env

--- local/callback.py
+++ local/callback.py
@@ -2,6 +2,8 @@
 """Callback script executed on the tester machine
 """
 import sys
+import misc, vmcheckerpaths
 
 if __name__ == "__main__":
-    print "[CALLBACK] YEAH! Callback ran! sys.argv[] = %s" % str(sys.argv)
+    print '[CALLBACK] YEAH! Callback ran! sys.argv[] = %s' % str(sys.argv)
+    print '[CALLBACK] test vmcheckerpaths-root: %s ' % vmcheckerpaths.root()
