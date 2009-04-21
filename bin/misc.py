d319657921a549a4db90ef10cb7b72c9f5751b15
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2009-04-22 00:54:21 +0300

    pylint misc.py + remove unused code

--- bin/misc.py
+++ bin/misc.py
@@ -1,16 +1,12 @@
 #! /usr/bin/env python2.5
 # -*- coding: utf-8 -*-
-
+"""Vmchecker configuration file readers"""
 
 from __future__ import with_statement
 
 __author__ = 'Alexandru Moșoi <brtzsnr@gmail.com>'
 
 
-import fcntl
-import os
-import socket
-import struct
 import ConfigParser
 
 import vmcheckerpaths
@@ -24,10 +20,10 @@ _tester_config = None
 
 def config_reader(config_file):
     """Returns a RawConfigParser for the speciffied file."""
-    config = ConfigParser.RawConfigParser()
+    config_ = ConfigParser.RawConfigParser()
     with open(config_file) as handle:
-        config.readfp(handle)
-    return config
+        config_.readfp(handle)
+    return config_
 
 
 def config():
@@ -42,9 +38,10 @@ def config_variables(config_file, section_name):
     """Return a dictionary with values from the config_path file.
 
     NB:  the keys will be all lowercase!
+
     """
-    config = config_reader(config_file)
-    return dict(config.items(section_name))
+    config_ = config_reader(config_file)
+    return dict(config_.items(section_name))
 
 
 def tester_config():
@@ -56,24 +53,6 @@ def tester_config():
             _tester_config.readfp(handle)
     return _tester_config
 
-
-def relative_path(*args):
-    """Joins the arguments and returns a path relative to root"""
-    return os.path.join(vmcheckerpaths.root(), os.path.join(*args))
-
-
-def get_ip_address(ifname):
-    """Returns ip address for network interface 'ifname'
-    in standard dotted notation.
-
-    Source from:
-        http://code.activestate.com/recipes/439094/
-    """
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', ifname[:15]))[20:24])
 
 
 def repository():
