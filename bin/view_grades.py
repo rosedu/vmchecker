3715e23ea7f38a4b424e30185bafd7885de00665
Alexandru Moșoi <brtzsnr@gmail.com>
2009-04-23 18:50:56 +0300

    fixed view grades to use the new schema

--- bin/view_grades.py
+++ bin/view_grades.py
@@ -1,14 +1,13 @@
-#! /usr/bin/env python2.5
-#! /usr/bin/env python2.5
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 """Generates a HTML table containing the students' grades.
 
-The layout can be modified by editing the following CSS elements: 
+The layout can be modified by editing the following CSS elements:
 
-table#hw-results-table {}           
+table#hw-results-table {}
 table#hw-results-table tr.tr-odd {} /* odd rows from table */
-table#hw-results-table tr.tr-even {}/* even rows from table */           
+table#hw-results-table tr.tr-even {}/* even rows from table */
 table#hw-results-table td.hw-h {}   /* home heading row */
 table#hw-results-table td.st-h {}   /* student heading column */
 table#hw-results-table td.grade {}  /* the grade cell */
@@ -18,10 +17,8 @@ __author__ = 'Gheorghe Claudiu-Dan <claudiugh@gmail.com>'
 
 
 import sqlite3
-import os
 import urllib
 import logging
-
 
 import config
 import vmcheckerpaths
@@ -30,34 +27,32 @@ import vmcheckerpaths
 _logger = logging.getLogger("vmchecker.view_grades")
 
 
-
-
 def get_db_content():
-    """ Retrieve all the information needed from the DB    
+    """ Retrieve all the information needed from the DB
     @return
-    a touple (results, hws) :
+    a touple (results, assignemnts):
     - a 2-D dictionary, having student names as rows and homework names as columns
     - a list containing all the homeworks in the DB
+
+    XXX remove assignments from return
     """
     global db_cursor
     results = {}
-    hws = []
+    assignments = config.assignments()
 
-    # first, get all the homeworks 
-    db_cursor.execute('SELECT nume, deadline from teme;')
-    for row in db_cursor.fetchall():
-        (hw_name, deadline) = row        
-        hws.append(hw_name)
-        
-    # build the 2-D dictionary, based on a JOIN selection  
-    db_cursor.execute('SELECT studenti.nume, teme.nume, nota FROM studenti, teme, note WHERE studenti.id = note.id_student AND teme.id = note.id_tema ;') 
-    for row in db_cursor.fetchall():
-        (student_name, hw_name, grade) = row
-        if not results.has_key(student_name):
-            results[student_name] = {}
-        results[student_name][hw_name] = grade
-        
-    return (results, hws)
+    # build the 2-D dictionary, based on a JOIN selection
+    db_cursor.execute(
+            'SELECT users.name, assignments.name, grades.grade '
+            'FROM users, assignments, grades '
+            'WHERE 1 '
+                'AND users.id = grades.user_id '
+                'AND assignments.id = grades.assignment_id')
+
+    for row in db_cursor:
+        user, assignment, grade = row
+        results.setdefault(user, {})[assignment] = grade
+
+    return results, assignments
 
 
 def href(target, text, title='Click pentru detalii'):
@@ -71,27 +66,27 @@ def href(target, text, title='Click pentru detalii'):
 def cpl_hack(student_name, hw_name, result):
     # TODO(alexandru): replace with something less specifi
     if result == '-1':
-    	result = 'ok'
+        result = 'ok'
     return "<a href=\"Teme/nota.php?user=%s&homework=%s\">%s</a>" % (
         urllib.quote(student_name), urllib.quote(hw_name), result)
 
 
 def gen_html(results, hws):
-    # table header 
+    # table header
     html = "<table id='hw-results-table'> <tr> <td > Nume </td> "
-    # the row with the name of the homeworks 
+    # the row with the name of the homeworks
     for hw_name in hws:
         html += "<td class='hw-h'> %s </td> \n" %hw_name
     html += "</tr>"
-    # table content 
+    # table content
     odd = True
     for student_name in sorted(results.keys()):
         if odd :
-            tr_class = 'tr-odd' 
-        else: 
+            tr_class = 'tr-odd'
+        else:
             tr_class = 'tr-even'
         html += "<tr class='%s'> <td class='st-h'> %s </td> " %(tr_class, student_name)
-        # for each student we generate a full row 
+        # for each student we generate a full row
         for hw_name in hws:
             html += '<td class="grade">'
             if results[student_name].has_key(hw_name):
@@ -103,19 +98,19 @@ def gen_html(results, hws):
         html += "</tr> \n"
         odd = not odd
     html += ' </table>'
-    return html 
+    return html
 
 
 def main():
     config.config_storer()
 
-    global db_connect, db_cursor
+    global db_cursor
     db_conn = sqlite3.connect(vmcheckerpaths.db_file())
     db_cursor = db_conn.cursor()
 
     (results, hws) = get_db_content()
-    # send to the stdout all the HTML content 
-    print (gen_html(results, hws))
+    # send to the stdout all the HTML content
+    print gen_html(results, hws)
     db_cursor.close()
     db_conn.close()
 
