1600518b15fce97c815ce13288ec7a5e1d5b58a9
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2009-05-03 11:24:28 +0300

    fixed calling cmd on windows

--- executor_scripts/so-win_vmchecker_run.sh
+++ executor_scripts/so-win_vmchecker_run.sh
@@ -10,10 +10,10 @@ run_tests()
     echo -e "\nchecker: testing"
 
     if [ -f _checker.bat ]; then
-        cmd /E:ON /V:ON /T:0E /C /c _checker.bat;
+        cmd /E:ON /V:ON /T:0E /C  _checker.bat;
     elif [ -f NMakefile.checker ]; then
         echo $winbuildenv \&\& nmake /nologo -f NMakefile.checker > __checker.bat;
-        cmd /E:ON /V:ON /T:0E /C /c __checker.bat;
+        cmd /E:ON /V:ON /T:0E /C  __checker.bat;
     elif [ -f _checker.sh ]; then
         sh _checker.sh;
     elif [ -f Makefile.checker ]; then
