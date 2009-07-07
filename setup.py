dc0eb5959fa08c81a48103415a06f8b6d3a6a8fc
vasile <vasile@vasile-desktop.(none)>
2009-07-07 15:25:14 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'vmchecker',
+      version = '0.3a1',
+      url = 'http://github.com/vmchecker/vmchecker/tree/master',
+      license = 'MIT',
+      package_dir = {'vmchecker': 'lib'},
+      scripts = [],
+     )
