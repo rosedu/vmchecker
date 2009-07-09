30e59b124be668939d427b7c10fd1a29eec45653
Alexandru Moșoi <brtzsnr@gmail.com>
2009-07-09 18:49:04 +0300

    removed old code

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,24 +1,8 @@
 from . import sql
 
+
 class VmcheckerError(Exception):
     pass
-
-
-class NoSuchAssignmentError(VmcheckerError):
-    pass
-
-class NoSuchUserError(VmcheckerError):
-    pass
-
-
-
-def get_assignment(assignment_id):
-    """Queries the database for the assignment identified by `assignment_id'"""
-    select = sql.assignments.select(sql.assignments.c.id == assignment_id)
-    result = select.execute().fetchone()
-    if result is None:
-        raise NoSuchAssignmentError('Invalid assignment id %d' % assignment_id)
-    return result
 
 
 def get_user(user_id):
@@ -28,7 +12,3 @@ def get_user(user_id):
     if result is None:
         raise NoSuchUserError
     return result
-
-
-__all__ = ['submit']
-
