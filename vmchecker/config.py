95187ba1d45a549193a156c151055df896f795aa
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2010-03-13 15:51:14 +0200

    config: remove dead code from config.py
    Signed-off-by: Lucian Adrian Grijincu <lucian.grijincu@gmail.com>

--- vmchecker/config.py
+++ vmchecker/config.py
@@ -7,7 +7,6 @@ from __future__ import with_statement
 
 import os
 import optparse
-import logging
 import ConfigParser
 
 import vmcheckerpaths
@@ -71,8 +70,6 @@ def parse_arguments():
 
 def _basic_config():
     """Common configuration"""
-    # sets logging
-    logging.basicConfig(level=logging.INFO)
     parse_arguments()
 
     # reads configuration
@@ -96,15 +93,3 @@ def config_tester():
     """Configures tester"""
     vmcfg = _basic_config()
     return vmcfg
-
-
-
-def _set_logging_level(option, opt_str, value, parser):
-    """Sets the logging level throu command-line arguments"""
-    setattr(parser.values, option.dest, True)
-
-    if option.dest == 'verbose':
-        logging.getLogger().setLevel(logging.DEBUG)
-    if option.dest == 'quiet':
-        logging.getLogger().setLevel(logging.WARN)
-
