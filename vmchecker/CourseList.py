bd659bb02f3971e486d0149eac20d72d33c35f7d
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2010-03-13 16:00:19 +0200

    CourseList: pylint
    Signed-off-by: Lucian Adrian Grijincu <lucian.grijincu@gmail.com>

--- vmchecker/CourseList.py
+++ vmchecker/CourseList.py
@@ -1,37 +1,49 @@
 #!/usr/bin/env python
 
+"""A module to interact with the course list"""
+
+
 DEFAULT_CONFIG_FILE_LIST = '/etc/vmchecker/config.list'
 
 class CourseList():
+    """A class to interact with the course list"""
     def __init__(self, config_file_list=DEFAULT_CONFIG_FILE_LIST):
+        """Create a CourseList object.
+        config_file_list - the path to the file containign the courses.
+        Defaults to DEFAULT_CONFIG_FILE_LIST
+        """
         course_list = {}
-        with open(config_file_list) as f:
-            lines = f.readlines()
+        with open(config_file_list) as handle:
+            lines = handle.readlines()
             for line in lines:
                 fields = line.split(':')
                 if len(fields) == 2:
                     course_list[fields[0].strip()] = fields[1].strip()
         self.course_list = course_list
 
-    def courseNames(self):
+    def course_names(self):
+        """Returns a list with the names of all courses"""
         return self.course_list.keys()
 
-    def courseConfigs(self):
+    def course_configs(self):
+        """Returns a list with the paths of all the config files for
+        all courses"""
         return self.course_list.values()
 
-    def courseConfig(self, courseId):
-        return self.course_list[courseId]
+    def course_config(self, course_id):
+        """Returns the config file for the specified course"""
+        return self.course_list[course_id]
 
-if __name__ == "__main__":
-    c = CourseList()
-    print c.courseNames()
-    print c.courseConfigs()
-    if len(c.courseNames()) > 0:
-        print c.courseConfig(c.courseNames()[0])
+def _test():
+    """Run a small test on CourseList"""
+    c_list = CourseList()
+    print c_list.course_names()
+    print c_list.course_configs()
+    if len(c_list.course_names()) > 0:
+        print c_list.course_config(c_list.course_names()[0])
     else:
         print "no courses :("
 
-            
-            
-            
-    
+
+if __name__ == "__main__":
+    _test()
