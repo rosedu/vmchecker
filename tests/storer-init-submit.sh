057cdf59c247bc9753b762879818287937f440eb
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2010-03-13 18:42:06 +0200

    tests: add a new test for storer testing
    Signed-off-by: Lucian Adrian Grijincu <lucian.grijincu@gmail.com>

---
+++ tests/storer-init-submit.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+DIR=/tmp/vm/
+ 
+rm -rdf $DIR && mkdir -p $DIR
+cd $DIR
+
+
+vmchecker-init-course storer
+vmchecker-init-course tester
+
+
+# create fake zip files for the test and user submission.
+touch file
+zip test.zip    file
+zip archive.zip file
+
+# place the test in the proper place
+cp test.zip tests/1-minishell-linux.zip
+
+vmchecker-submit SO 1-minishell-linux u1 archive.zip 
+vmchecker-submit SO 1-minishell-linux u1 archive.zip  # resubmit!
+vmchecker-submit SO 1-minishell-linux u2 archive.zip 
+vmchecker-submit SO 1-minishell-linux u3 archive.zip 
+
+vmchecker-resubmit -c SO --all
+vmchecker-update-db -c SO -all
+
+vmchecker-update-db -c SO --simulate --all
