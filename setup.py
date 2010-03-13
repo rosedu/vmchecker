07105cbffca4a13c4a945eaf9f3e9835788f3241
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2010-03-13 18:16:44 +0200

    view-grades: converted to script
    Signed-off-by: Lucian Adrian Grijincu <lucian.grijincu@gmail.com>

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(name = 'vmchecker',
                  'bin/vmchecker-submit',
                  'bin/vmchecker-resubmit',
                  'bin/vmchecker-update-db',
+                 'bin/vmchecker-view-grades',
                  ],
       data_files = [('/etc/init.d/', ['etc/init.d/vmchecker']),
 	            ('/etc/vmchecker/', ['etc/vmchecker/config.list'])])
