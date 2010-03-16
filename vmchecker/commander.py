9598f716fadcbfacc4b7db796495688ff67e51d0
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2010-03-16 02:15:13 +0200

    commander: refactored a bit
    Signed-off-by: Lucian Adrian Grijincu <lucian.grijincu@gmail.com>

--- vmchecker/commander.py
+++ vmchecker/commander.py
@@ -53,17 +53,120 @@ def _run_callback(bundle_dir):
     callback.run_callback(os.path.join(bundle_dir, 'config'), abs_files)
 
 
-def _make_test_config(vmcfg, assignment, bundle_dir):
+
+
+
+def _run_executor(json_cfg_fname, executor_job_dir, assignment, timeout):
+    """Starts a job.
+
+    Allthough it would be nicer to import vm-executor and just invoke
+    methods from there, the vm-executor can get stuck (due to fauly
+    pyvix interaction with the vmware server 1 daemon). We're already
+    called directly from queue-manager. If we get stuck too, the whole
+    tester-side vmchecker gets stuck.
+
+    Because of this, vm-executor is launched in a separate process.
+    """
+
+    # get the current time
+    start = time.time()
+    deadline = start + int(timeout) + _EXECUTOR_OVERHEAD
+
+
+    # first just try to open the process
+    try:
+        cmd = ['vmchecker-vm-executor', json_cfg_fname]
+        _logger.info('Begin homework evaluation. Calling %s', cmd)
+
+        popen = Popen(cmd)
+
+        with open(os.path.join(executor_job_dir, 'job_results'), 'w') as handler:
+            print >> handler, 'ok'
+    except OSError:
+        _logger.exception('Cannot invoke VMExecutor.')
+        with open(os.path.join(executor_job_dir, 'job_errors'), 'a') as handler:
+            print >> handler, 'Cannot run VMExecutor.'
+            print >> handler, 'Please contact the administrators.'
+        # if we cannot open the process, there is nothing more to be done
+        return
+
+
+
+
+    # waits for the the process to finish
+    try:
+        counter = 0
+        while time.time() < deadline:
+            counter += 1
+            exit_code = popen.poll()
+
+            if exit_code is None:
+                # if process has not finished => continue to sleep
+                _logger.debug('-- VMExecutor sleeping for 5 seconds, '
+                              'exit_code is None: x=%d', counter)
+                # polls every 5 seconds
+                time.sleep(5)
+            else:
+                with open(os.path.join(executor_job_dir, 'job_errors'), 'a') as handler:
+                    print >> handler, 'VMExecutor returned %d (%s)' % (
+                        exit_code, ['success', 'error'][exit_code < 0])
+
+                # no reason to stay in the loop after process exit terminates
+                popen = None
+                return
+        else:
+            _logger.error("VMChecker timeouted on assignment `%s'" % assignment)
+            with open(os.path.join(executor_job_dir, 'job_errors'), 'a') as handler:
+                print >> handler, """\ VMExecutor successfuly started,
+                      but it's taking too long. Check your sources,
+                      makefiles, etc and resubmit.  If the problem
+                      persists please contact administrators."""
+    except:
+        _logger.exception('Exception after starting VMExecutor.')
+        with open(os.path.join(executor_job_dir, 'job_errors'), 'a') as handler:
+            print >> handler, """\ Error after starting VMExecutor.
+                  If the problem persists please contact
+                  administrators."""
+    finally:
+        # release any leftover resources
+        try:
+            if not popen is None:
+                import signal
+                os.kill(popen.pid, signal.SIGTERM)
+                # can't do "popen.kill()" here because that's
+                # python2.6 speciffic :(
+        except:
+            _logger.exception('Nested exception in kill(PID=%d)', popen.pid)
+
+
+
+
+def _check_required_files(path):
+    """Checks that a set of files required by commander is present in
+    the given path."""
+    found_all = True
+    needed_files = ['archive.zip', 'tests.zip', 'config']
+    found_files = os.listdir(path)
+    for need in needed_files:
+        if not need in found_files:
+            _logger.error('Could not find necessary file [%s] in [%s]' % (
+                    need, path))
+            found_all = False
+    if not found_all:
+        exit(-1)
+
+
+
+def _make_test_config(bundle_dir, assignment, vmcfg, asscfg):
     """Returns an object with a configuration suitable for
     vm-executor"""
-    asscfg  = vmcfg.assignments()
-    machine = asscfg.get(assignment, 'Machine')
     timeout = asscfg.get(assignment, 'Timeout')
-    kernel_messages = asscfg.get(assignment, 'KernelMessages')
+    machine = asscfg.get(assignment, 'Machine')
+    kernel_messages_str = asscfg.get(assignment, 'KernelMessages')
 
-    km = True if int(kernel_messages) != 0 else False
+    kernel_messages = True if int(kernel_messages_str) != 0 else False
     return {
-        'km_enable' : km,
+        'km_enable' : kernel_messages,
         'host' : {
             'vmx_path'       : vmcfg.get(machine, 'VMPath'),
             'vmchecker_root' : vmcfg.root_path(),
@@ -96,104 +199,11 @@ def _make_test_config(vmcfg, assignment, bundle_dir):
         }
 
 
-
-def _run_executor(json_cfg_fname, executor_job_dir, assignment, timeout):
-    """Starts a job.
-
-    XXX lots of wtf per minute
-    XXX parsing config should be executors' job
-
-    """
-    args = ['vmchecker-vm-executor', json_cfg_fname]
-    _logger.info('Begin homework evaluation')
-    _logger.debug('calling %s', args)
-
-    start = time.time()
-
-    # first just try to open the process
-    try:
-        popen = Popen(args)
-    except OSError:
-        _logger.exception('Cannot invoke VMExecutor.')
-        with open(os.path.join(executor_job_dir, 'job_errors'), 'a') as handler:
-            print >> handler, 'Cannot run VMExecutor.'
-            print >> handler, 'Please contact the administrators.'
-        # if we cannot open the process, there is nothing more to be done
-        return
-
-    with open(os.path.join(executor_job_dir, 'job_results'), 'w') as handler:
-        print >> handler, 'ok'
-
-    # waits for the the process to finish
-    try:
-        counter = 0
-        deadline = start + int(timeout) + _EXECUTOR_OVERHEAD
-
-        while time.time() < deadline:
-            counter += 1
-            exit_code = popen.poll()
-
-            if exit_code is None:
-                # if process has not finished => continue to sleep
-                _logger.debug('-- VMExecutor sleeping for 5 seconds, '
-                              'exit_code is None: x=%d', counter)
-                # polls every 5 seconds
-                time.sleep(5)
-            else:
-                with open(os.path.join(executor_job_dir, 'job_errors'), 'a') as handler:
-                    print >> handler, 'VMExecutor returned %d (%s)' % (
-                        exit_code, ['success', 'error'][exit_code < 0])
-
-                # no reason to stay in the loop after process exit terminates
-                popen = None
-                return
-        else:
-            _logger.error("VMChecker timeouted on assignment `%s'" % assignment)
-
-            with open(os.path.join(executor_job_dir, 'job_errors'), 'a') as handler:
-                print >> handler, """\ VMExecutor successfuly started,
-                      but it's taking too long. Check your sources,
-                      makefiles, etc and resubmit.  If the problem
-                      persists please contact administrators."""
-    except:
-        _logger.exception('Exception after starting VMExecutor.')
-
-        with open(os.path.join(executor_job_dir, 'job_errors'), 'a') as handler:
-            print >> handler, """\ Error after starting VMExecutor.
-                  If the problem persists please contact
-                  administrators."""
-    finally:
-        # release any leftover resources
-        try:
-            if not popen is None:
-                import signal
-                os.kill(popen.pid, signal.SIGTERM)
-                # can't do "popen.kill()" here because that's
-                # python2.6 speciffic :(
-        except:
-            pass
-
-
-def _check_required_files(path):
-    """Checks that a set of files required by commander is present in
-    the given path."""
-    found_all = True
-    needed_files = ['archive.zip', 'tests.zip', 'config']
-    found_files = os.listdir(path)
-    for need in needed_files:
-        if not need in found_files:
-            _logger.error('Could not find necessary file [%s] in [%s]' % (
-                    need, path))
-            found_all = False
-    if not found_all:
-        exit(-1)
-
-
-def _write_test_config(dst_file, vmcfg, assignment, bundle_dir):
+def _write_test_config(dst_file, bundle_dir, assignment, vmcfg, asscfg):
     """Write the test configuration to a json file to be passed in to
     the vm-executor"""
     with open(dst_file, 'w') as handler:
-        testcfg = _make_test_config(vmcfg, assignment, bundle_dir)
+        testcfg = _make_test_config(bundle_dir, assignment, vmcfg, asscfg)
         testcfg_str = json.write(testcfg)
         handler.write(testcfg_str)
 
@@ -207,15 +217,21 @@ def _get_assignment_id(bundle_dir):
     assignment = config.get('Assignment', 'Assignment')  # yet another hack
     return assignment
 
+
+
 def prepare_env_and_test(vmcfg, bundle_dir):
     """Prepare testing environment for vm-executor, create a config
     file and run vm-executor"""
 
     _check_required_files(bundle_dir)
+
     assignment = _get_assignment_id(bundle_dir)
+    asscfg  = vmcfg.assignments()
+    timeout = asscfg.get(assignment, 'Timeout')
+
 
     json_cfg_fname = os.path.join(bundle_dir, 'vm_executor_config.json')
-    _write_test_config(json_cfg_fname, assignment, bundle_dir, vmcfg)
+    _write_test_config(json_cfg_fname, bundle_dir, assignment, vmcfg, asscfg)
 
     try:
         _run_executor(json_cfg_fname, bundle_dir, assignment, timeout)
