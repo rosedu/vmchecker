145a032422d6217106715dc0cb7cdb89a43c38c1
aaasz <aaa_sz@yahoo.com>
2010-03-17 19:19:27 +0200

    Modified services

--- web_services/services.py
+++ web_services/services.py
@@ -1,26 +1,48 @@
 """
-   File: services.py
-   Date: February 2010
-  
-   This script implements the VMChecker's Web Services.
-   It is based on apache2 and mod_python.
+This script implements the VMChecker's Web Services.
+It's based on apache2 and mod_python.
 """
+
+
+# Use simplejson or Python 2.6 json, prefer simplejson.
+try:
+    import simplejson as json
+except ImportError:
+    import json
 
 import os
 import sys
 import tempfile
 import subprocess
 import time
+import traceback
 
 import ConfigParser
-import json
 from mod_python import Cookie, apache, Session
 
-#import config
-#import vmcheckerpaths
+from vmchecker.courselist import CourseList
+from vmchecker import submit, config
 
-AUTH_DB = [{'username' : 'vmchecker', 
+
+# define ERROR_MESSAGES
+ERR_ACCESS_DENIED = "Access denied."
+ERR_FILE_NOT_UPLOADED = "File not uploaded."
+ERR_EXCEPTION = "An exception occured on our server. Please notify the administrators."
+
+
+AUTH_DB = [{'username' : 'vmchecker',
            'password' : 'vmchecker'}]
+
+class OutputString():
+	def __init__(self):
+		self.st = ""
+
+	def write(self, st):
+		self.st += st
+	
+	def get(self):
+		return self.st 
+
 
 def get_user(credentials):
 	#XXX : Cu LDAP
@@ -35,77 +57,99 @@ def get_user(credentials):
 def fbuffer(f, chunk_size=10000):
 	while True:
 		chunk = f.read(chunk_size)
-		if not chunk: break
+		if not chunk: 
+			break
 		yield chunk
 
-########## @ServiceMethod
-def uploadAssignment(req, courseid, assignmentid, username, archivefile):
-	""" Saves a temp file of the uploaded archive and calls
-		submit.py to put the homework in the testing queue"""
 
+########## @ServiceMethod
+def uploadAssignment(req, courseid, assignmentid, archivefile):
+	""" Saves a temp file of the uploaded archive and calls
+        vmchecker.submit.submit method to put the homework in
+		the testing queue"""
+	
+	# Check permission
 	s = Session.Session(req)
 	if s.is_new():
 		s.invalidate()
-		return apache.HTTP_FORBIDDEN
+		return json.dumps({"error":ERR_ACCESS_DENIED})
 
+	s.load()
+	try:
+		username = s['username']
+	except:
+		return json.dumps({"error":ERR_EXCEPTION + ": " + 
+						"Could not fetch the username session variable."})  	
+	
 	# Reset the timeout
 	s.save()
-	
+
+	# Get the archive name
 	fileitem = req.form['archivefile']
 
 	# Test if the file was uploaded
 	if fileitem.filename == None:
-		return  json.dumps({'status':None,
-			'dumpLog':'No file has been received'})
+		return  json.dumps({'error':ERR_FILE_NOT_UPLOADED})
 
-	###  Save file in a temp  
+	#  Save file in a temp
 	fd, tmpname = tempfile.mkstemp('.zip')
 	f = open(tmpname, 'wb', 10000)
-	# Read the file in chunks
+    ## Read the file in chunks
 	for chunk in fbuffer(fileitem.file):
 		f.write(chunk)
 	f.close()
 
-	###  Call submit.py
-	#TODO xxx de-harcode
-	#config.config_storer() 	
-	#s_path = os.path.join(vmcheckerpaths.dir_bin(),'submit.py')
-	s_path = '/home/szekeres/Desktop/vmchecker/bin/submit.py'
+	# Call submit.py
+	## Redirect stdout
+	strout = OutputString()
+	sys.stdout = strout
 	
-	process = subprocess.Popen(['python', s_path, assignmentid, 
-			username, tmpname],
-			shell=False, 
-			stderr=subprocess.STDOUT, 
-			stdout=subprocess.PIPE)
-	
-	dumpLog = process.communicate()[0]
-	status = process.returncode		
+	try:
+		status = submit.submit(tmpname, assignmentid, 
+							username, courseid)
+	except:
+		traceback.print_exc(file = strout)
+		return json.dumps({'error':ERR_EXCEPTION + " : " + strout.get()})
 	
 	return json.dumps({'status':status,
-			'dumpLog':dumpLog})
-	
+            'dumpLog':strout.get()}) 
+
+
 ########## @ServiceMethod
-def getResults(req, courseid, assignmentid, username): 
+def getResults(req, courseid, assignmentid):
 	""" Returns the result for the current user"""
 
+	# Check permission 	
 	s = Session.Session(req)
 	if s.is_new():
 		s.invalidate()
-		return apache.HTTP_FORBIDDEN
-	
+		return json.dumps({"error":ERR_ACCESS_DENIED})
+
+	# Get username session variable
+	s.load()
+	try:
+		username = s['username']
+	except:
+		return json.dumps({"error":ERR_EXCEPTION + ": " + \
+					"Could not fetch the username session variable."})  	
+
+	strout = OutputString()
+	try:
+		vmcfg = config.VmcheckerConfig(CourseList().course_config(courseid))
+	except:
+		traceback.print_exc(file = strout)
+		return json.dumps({'error':ERR_EXCEPTION + " : " + strout.get()})
+				
+	r_path =  vmcfg.repository_path() + "/" + assignmentid + \
+										"/" + username + "/results/"
+
 	# Reset the timeout
 	s.save()
-	
-	#TODO xxx de-hardcode	
-	#config.config_storer()
-	#r_path = vmcheckerpaths.dir_results(courseid, assignmentid, username)
-	r_path = "/home/szekeres/vmchecker/repo/" + courseid + \
-			"/" + assignmentid +"/" + username + "/results/"
 
 	if not os.path.isdir(r_path):
-		#TODO fortune 
-		#TODO cand se updateaza baza de date?	
-		return json.dumps({'resultLog':'not yet'});
+		#TODO fortune
+		#TODO cand se updateaza baza de date?
+		return json.dumps({'resultLog':'The results are not ready.'});
 	else:
 		resultlog = ""
 		for fname in os.listdir(r_path):
@@ -113,9 +157,10 @@ def getResults(req, courseid, assignmentid, username):
 			if os.path.isfile(f_path):
 				f = open(f_path, "r")
 				resultlog += "===== " + fname + " =====\n"
-				resultlog += f.read()		
+				resultlog += f.read()
 		return json.dumps({'resultlog':resultlog})
-	
+
+
 ######### @ServiceMethod
 def getCourses(req):
 	""" Returns a JSON object containing the list of available courses """
@@ -123,17 +168,25 @@ def getCourses(req):
 	s = Session.Session(req)
 	if s.is_new():
 		s.invalidate()
-		return apache.HTTP_FORBIDDEN
-	
+		return json.dumps({"error":ERR_ACCESS_DENIED})
+
 	# Reset the timeout
 	s.save()
-	
-	#TODO xxx Dupa ce termina Lucian
-	return json.dumps([{'id':'so','title':'Sisteme de Operare'},
-		{'id':'pso','title':'Sisteme de Operare 2'},
-		{'id':'cpl','title':'Compilatoare'},
-		{'id':'pa','title':'Proiectarea Algoritmilor'}])
-	
+
+	strout = OutputString()
+	try:
+		clist = CourseList()
+	except:
+		traceback.print_exc(file = strout)
+		return json.dumps({'error':ERR_EXCEPTION + " : " + strout.get()})
+				
+	course_arr = []
+	for course_id in clist.course_names():
+		course_arr.append({'id' : course_id,
+			'title' : course_id}) # XXX: TODO: get a long name
+	return json.dumps(course_arr)
+
+
 ######### @ServiceMethod
 def getAssignments(req, courseid):
 	""" Returns the list of assignments for a given course """
@@ -141,51 +194,52 @@ def getAssignments(req, courseid):
 	s = Session.Session(req)
 	if s.is_new():
 		s.invalidate()
-		return apache.HTTP_FORBIDDEN
-	
+		return json.dumps({"error":ERR_ACCESS_DENIED})
+
 	# Reset the timeout
 	s.save()
-	
-	#TODO Nu stiu inca layoutul final al fisierelor de configurare - dupa ce termina Lucian
-	#TODO xxx path to .vmcheckerrc for this courseid
-	#config.config_storer()
-	c_path = '/home/szekeres/storer.ini'
-	parser = ConfigParser.RawConfigParser()
-	assignments = []      
-	if parser.read(c_path):
-		for section in parser.sections():
-			s = section.split() 	
-			if s[0] == 'assignment' and s[1] !=  'DEFAULT':
-			#for each assignment section
-				a = {}
-				a['assignmentId'] = s[1]
-				a['assignmentTitle'] = parser.get(section, 'Title')
-				a['deadline'] = parser.get(section, 'Deadline')
-				assignments.append(a)
-		return json.dumps(assignments)	
-	else:
-	#'The configuration file doesn't exist'
-		return json.dumps(None) 	
-	
+
+	strout = OutputString()
+	try:
+		vmcfg = config.VmcheckerConfig(CourseList().course_config(courseid))
+	except:
+		traceback.print_exc(file = strout)
+		return json.dumps({'error':ERR_EXCEPTION + " : " + strout.get()})
+
+	assignments = vmcfg.assignments()
+	ass_arr = []
+
+	for key in assignments.__assignments:
+		a = {}
+		a['assignmentId'] = key
+		a['assignmentTitle'] = assignments.get(key, "assignmentTitle")
+		a['deadline'] = assignments.get(key, "deadline")
+		ass_arr.append(a)
+	return json.dumps(ass_arr)
+
+
 ######### @ServiceMethod
 def login(req, username, password):
-	s = Session.Session(req)
-	if not s.is_new():
-		return "Not supposed to be here, you're already logged in"
-		
-	user = get_user({'username' : username, 'password' : password})
-	if user is None:
-		s.invalidate()
-		return "You're not from these places"
-	
-	s["username"] = username
-	s.save()
-	return "You are logged in"
-	
+    s = Session.Session(req)
+
+    if not s.is_new():
+    	return json.dumps({'status':'true', 'username':username,
+									'info':'Already logged in'})
+    
+    user = get_user({'username' : username, 'password' : password})
+    if user is None:
+        s.invalidate()
+        return json.dumps({'status':'false', 'username':None, 
+									'info':'Invalid username/password'})
+
+    s["username"] = username
+    s.save()
+    return json.dumps({'status':'true', 'username':username,
+									'info':'Succesfully logged in'})
+
+
 ######### @ServiceMethod
 def logout(req):
     s = Session.Session(req)
     s.invalidate()
-    return "You logged out"
-    	
-   
+
