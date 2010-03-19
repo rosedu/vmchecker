51eeb8c0a7740b217160feb89bb430340c700149
Adriana Szekeres <aaa_sz@yahoo.com>
2010-03-19 23:04:33 +0200

    add clean.sh
    Signed-off-by: Adriana Szekeres <aaa_sz@yahoo.com>

---
+++ clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -rf /usr/local/bin/vmchecker-* 
+rm -rf /usr/bin/vmchecker-*
+
+rm -rf /usr/local/lib/python2.6/dist-packages/vmchecker/*
+rm -rf /usr/lib/python2.6/dist-packages/vmchecker/*
+
+rm -rf /usr/local/lib/python2.6/vmchecker/*
+rm -rf /usr/lib/python2.6/vmchecker/*
+
+rm -rf /etc/vmchecker/
+rm -f /etc/init.d/vmchecker
+
