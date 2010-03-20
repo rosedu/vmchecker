996a39a214010c5b7a0c5fee1a2fcd092401d678
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2010-03-20 17:24:11 +0200

    clean: clean python2.5 files too
    Signed-off-by: Lucian Adrian Grijincu <lucian.grijincu@gmail.com>

--- clean.sh
+++ clean.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 rm -rf /usr/local/bin/vmchecker-* 
 rm -rf /usr/bin/vmchecker-*
 
@@ -8,6 +9,12 @@ rm -rf /usr/lib/python2.6/dist-packages/vmchecker/*
 
 rm -rf /usr/local/lib/python2.6/vmchecker/*
 rm -rf /usr/lib/python2.6/vmchecker/*
+
+rm -rf /usr/local/lib/python2.5/dist-packages/vmchecker/*
+rm -rf /usr/lib/python2.5/dist-packages/vmchecker/*
+
+rm -rf /usr/local/lib/python2.5/vmchecker/*
+rm -rf /usr/lib/python2.5/vmchecker/*
 
 rm -rf /etc/vmchecker/
 rm -f /etc/init.d/vmchecker
