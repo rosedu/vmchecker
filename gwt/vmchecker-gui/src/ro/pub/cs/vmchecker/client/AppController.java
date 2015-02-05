a00905d76ad780ed53e95e942b926f147f374b64
Calin Iorgulescu <calin.iorgulescu@gmail.com>
2015-02-05 12:17:21 +0100

    gwt: Remove some useless imports. Minor whitespace changes.

--- gwt/vmchecker-gui/src/ro/pub/cs/vmchecker/client/AppController.java
+++ gwt/vmchecker-gui/src/ro/pub/cs/vmchecker/client/AppController.java
@@ -26,7 +26,6 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerManager;
-import com.google.gwt.user.client.Cookies;
 import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.HistoryListener;
 import com.google.gwt.user.client.Window;
